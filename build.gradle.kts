bf4c95ba17b729b0e7dc0e77abad17976e115d90
Álvaro Salcedo García <alvaro@alvr.dev>
2022-01-14 18:46:11 +0100

    feat(project): add Detekt analyzer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
+    alias(libs.plugins.detekt)
     alias(libs.plugins.updates)
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 }
 
 tasks.register<Delete>("clean") {
@@ -15,4 +21,28 @@ tasks.withType<DependencyUpdatesTask> {
             else -> checkDependencyVersion(candidate.version).isLessStableThan(current)
         }
     }
+}
+
+tasks.register<Detekt>("detektAll") {
+    description = "Run detekt in all modules"
+
+    parallel = true
+    ignoreFailures = false
+    autoCorrect = true
+    buildUponDefaultConfig = true
+    setSource(files(projectDir))
+    config.setFrom(files("$rootDir/config/detekt.yml"))
+    include("**/*.kt", "**/*.kts")
+    exclude("**/resources/**", "**/build/**")
+
+    reports {
+        html.required.set(true)
+        sarif.required.set(false)
+        txt.required.set(false)
+        xml.required.set(false)
+    }
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
 }
