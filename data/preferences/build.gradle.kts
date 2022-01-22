037a1609490c73c28174e34f28c355423b2d087d
Álvaro Salcedo García <alvaro@alvr.dev>
2022-01-22 09:50:57 +0100

    feat(poject): configure protobuf gradle plugin

--- data/preferences/build.gradle.kts
+++ data/preferences/build.gradle.kts
@@ -1,9 +1,30 @@
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.plugins
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
 plugins {
     id("modules.android-library")
+    alias(libs.plugins.protobuf)
+}
+
+protobuf {
+    protoc {
+        artifact = "${libs.protobuf.protoc.get()}"
+    }
+
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("java").option("lite")
+            }
+        }
+    }
 }
 
 dependencies {
     implementation(libs.bundles.common.android)
+    implementation(libs.bundles.data.preferences)
 
     testImplementation(libs.bundles.test)
 
