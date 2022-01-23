58149356969b3e43f4827c8d0d649c27c9ba3ba6
Álvaro Salcedo García <alvaro@alvr.dev>
2022-01-23 10:09:29 +0100

    feat(project): add kover plugin for code coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
+import kotlinx.kover.api.CoverageEngine
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import utils.KOVER_MIN_COVERED_LINES
 import utils.configureKotlin
+import utils.koverExcludes
+import utils.koverIncludes
 
 plugins {
     plugins.dependencies
     plugins.detekt
+    alias(libs.plugins.kover)
 }
 
 // Version catalogs is not accessible from precompile scripts
@@ -12,10 +17,33 @@ buildscript {
     extra.set("detektFormatting", libs.detekt.formatting)
 }
 
+tasks.koverMergedHtmlReport {
+    includes = koverIncludes
+    excludes = koverExcludes
+}
+
+tasks.koverMergedVerify {
+    includes = koverIncludes
+    excludes = koverExcludes
+
+    rule {
+        name = "Minimal line coverage rate in percent"
+        bound {
+            minValue = KOVER_MIN_COVERED_LINES
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(buildDir)
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.configureKotlin()
+}
+
+kover {
+    coverageEngine.set(CoverageEngine.INTELLIJ)
+    jacocoEngineVersion.set(libs.versions.intellij.get())
+    instrumentAndroidPackage = true
 }
---
+++ buildSrc/src/main/kotlin/utils/kover.kt
@@ -0,0 +1,6 @@
+package utils
+
+val koverIncludes = listOf("dev.alvr.katana.*")
+val koverExcludes = emptyList<String>()
+
+const val KOVER_MIN_COVERED_LINES = 50
