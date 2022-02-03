b3f192964186b1b2e4fde8f2e2d0fd9adae51bc0
Álvaro Salcedo García <alvaro@alvr.dev>
2022-02-03 18:39:31 +0100

    feat(login): add dagger kapt to module

--- ui/login/build.gradle.kts
+++ ui/login/build.gradle.kts
@@ -3,9 +3,10 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.lifecycle.viewmodel)
     implementation(libs.bundles.common.android)
     implementation(libs.bundles.ui.compose)
+
+    kapt(libs.bundles.kapt.ui)
 
     debugImplementation(libs.compose.ui.test.manifest)
     testImplementation(libs.bundles.test)
