93e2fcd0b5ca18419d02728783f021eb019e0eac
Álvaro Salcedo García <alvaro@alvr.dev>
2022-06-05 10:27:35 +0200

    feat(nav): refactor `KatanaNavigator` to Destinations

--- app/src/main/kotlin/dev/alvr/katana/MainActivity.kt
+++ app/src/main/kotlin/dev/alvr/katana/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
-import dev.alvr.katana.navigation.KatanaNavigator
+import dev.alvr.katana.navigation.KatanaDestinations
 import dev.alvr.katana.ui.base.Katana
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Katana {
-                KatanaNavigator()
+                KatanaDestinations()
             }
         }
     }
---
+++ app/src/main/kotlin/dev/alvr/katana/MainViewModel.kt
@@ -0,0 +1,21 @@
+package dev.alvr.katana
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.alvr.katana.domain.base.usecases.sync
+import dev.alvr.katana.domain.token.usecases.GetAnilistTokenUseCase
+import dev.alvr.katana.navigation.NavGraphs
+import dev.alvr.katana.ui.login.navigation.LoginNavGraph
+import javax.inject.Inject
+
+@HiltViewModel
+internal class MainViewModel @Inject constructor(
+    private val getAnilistTokenUseCase: GetAnilistTokenUseCase,
+) : ViewModel() {
+    val initialRoute
+        get() = if (getAnilistTokenUseCase.sync() != null) {
+            NavGraphs.home
+        } else {
+            LoginNavGraph
+        }
+}
