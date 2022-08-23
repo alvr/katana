77c4047f013f6f972002c6e9aef357e57f655e1f
Álvaro Salcedo García <alvaro@alvr.dev>
2022-08-23 16:09:56 +0200

    chore: merge TestReport task with `unitTests`

---
+++ common/tests-android/src/main/kotlin/dev/alvr/katana/common/tests/SdkVersions.kt
@@ -0,0 +1,8 @@
+package dev.alvr.katana.common.tests
+
+import android.os.Build
+
+internal object SdkVersions {
+    const val MIN = Build.VERSION_CODES.P
+    const val TARGET = Build.VERSION_CODES.S_V2
+}
