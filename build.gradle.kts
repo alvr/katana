764753c2c2cc9f0238bc4b0babf7cac6ee874d8e
Álvaro Salcedo García <alvaro@alvr.dev>
2023-03-23 22:29:03 +0100

    chore: retry for flaky tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
+import com.gradle.enterprise.gradleplugin.testretry.retry
+
 plugins {
     id("katana.common")
     id("katana.dependency-versions")
     id("katana.detekt")
     id("katana.kover")
     id("katana.sonarqube")
+}
+
+tasks.withType<Test>().configureEach {
+    retry {
+        maxRetries.set(3)
+        failOnPassedAfterRetry.set(true)
+    }
 }
