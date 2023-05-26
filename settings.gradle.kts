a03bc10dba0ac1cd6d6bbdd94c08bafe58d75038
Álvaro Salcedo García <alvaro@alvr.dev>
2023-05-26 20:44:03 +0200

    fix: gradle repositories mode

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "Katana"
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
