e352c03f4e1aaf6b7617ed54ae2328dd0126e6f2
Álvaro Salcedo García <alvaro@alvr.dev>
2023-10-14 11:02:02 +0200

    chore: replace accompanist placeholder

--- ui/base/src/iosMain/kotlin/dev/alvr/katana/ui/base/modifiers/KatanaPlaceholder.kt
+++
@@ -1,9 +0,0 @@
-package dev.alvr.katana.ui.base.modifiers
-
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
-
-actual fun Modifier.katanaPlaceholder(
-    visible: Boolean,
-    shape: Shape?,
-) = this
