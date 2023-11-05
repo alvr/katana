bde8876d206ae5c0379e8b4f5f4761855692112f
Álvaro Salcedo García <alvaro@alvr.dev>
2023-11-05 11:51:47 +0100

    fix: use KatanaResources on `.webp` and `.xml` only

--- ui/base/src/androidMain/kotlin/dev/alvr/katana/ui/base/design/typography.android.kt
+++ ui/base/src/androidMain/kotlin/dev/alvr/katana/ui/base/design/typography.android.kt
@@ -6,16 +6,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import dev.alvr.katana.ui.base.resources.KatanaResource
 
 private val cache = mutableMapOf<String, Font>()
 
 @Composable
 @SuppressLint("DiscouragedApi")
-internal actual fun font(font: KatanaResource, weight: FontWeight, style: FontStyle) =
+internal actual fun font(font: String, weight: FontWeight, style: FontStyle) =
     with(LocalContext.current) {
-        cache.getOrPut(font.id) {
-            val id = resources.getIdentifier(font.id, "font", packageName)
+        cache.getOrPut(font) {
+            val id = resources.getIdentifier(font, "font", packageName)
             Font(id, weight, style)
         }
     }
