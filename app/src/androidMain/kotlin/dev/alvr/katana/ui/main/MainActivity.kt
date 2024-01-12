531db964c76d1a98fdb342ed27abf8f563cd660b
Álvaro Salcedo García <alvaro@alvr.dev>
2024-01-12 17:43:13 +0100

    feat: splashscreen in Android

--- app/src/androidMain/kotlin/dev/alvr/katana/ui/main/MainActivity.kt
+++ app/src/androidMain/kotlin/dev/alvr/katana/ui/main/MainActivity.kt
@@ -3,10 +3,12 @@ package dev.alvr.katana.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 
 internal class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent { KatanaApp() }
