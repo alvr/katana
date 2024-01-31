e083c9b9b1db8f630f085d27aed0258acfa4c6a0
Álvaro Salcedo García <alvaro@alvr.dev>
2024-01-31 14:17:01 +0100

    chore: rename action

---
+++ init.gradle.kts
@@ -0,0 +1,14 @@
+import org.gradle.github.GitHubDependencyGraphPlugin
+
+initscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("org.gradle:github-dependency-graph-gradle-plugin:1.2.0")
+    }
+}
+
+apply<GitHubDependencyGraphPlugin>()
