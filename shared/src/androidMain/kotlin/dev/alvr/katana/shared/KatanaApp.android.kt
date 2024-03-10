1b960837c31843c2d9fd0e971f284d352c6b4503
Álvaro Salcedo García <alvaro@alvr.dev>
2024-03-10 18:08:33 +0100

    chore: enable strong skipping

--- shared/src/androidMain/kotlin/dev/alvr/katana/shared/KatanaApp.android.kt
+++ shared/src/androidMain/kotlin/dev/alvr/katana/shared/KatanaApp.android.kt
@@ -1,17 +1,14 @@
 package dev.alvr.katana.shared
 
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import dev.alvr.katana.core.ui.utils.isLandscape
 import dev.alvr.katana.shared.navigation.KatanaDestinations
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 internal actual fun KatanaContent() {
     KatanaDestinations(
-        useNavRail = calculateWindowSizeClass().widthSizeClass > WindowWidthSizeClass.Medium,
+        useNavRail = isLandscape(),
         vm = koinViewModel(),
     )
 }
