7beb8be04363462b9d897b5368386743a6930095
Álvaro Salcedo García <alvaro@alvr.dev>
2024-04-13 17:30:57 +0200

    chore: do not use `composed` modifier

--- core/ui/src/commonMain/kotlin/dev/alvr/katana/core/ui/modifiers/KatanaPlaceholder.kt
+++ core/ui/src/commonMain/kotlin/dev/alvr/katana/core/ui/modifiers/KatanaPlaceholder.kt
@@ -1,19 +1,25 @@
 package dev.alvr.katana.core.ui.modifiers
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Shape
+import com.eygraber.compose.placeholder.PlaceholderDefaults
 import com.eygraber.compose.placeholder.PlaceholderHighlight
 import com.eygraber.compose.placeholder.material3.placeholder
-import com.eygraber.compose.placeholder.material3.shimmer
+import com.eygraber.compose.placeholder.material3.shimmerHighlightColor
+import com.eygraber.compose.placeholder.shimmer
 
+@Composable
+@Suppress("ModifierComposable")
 fun Modifier.katanaPlaceholder(
     visible: Boolean,
     shape: Shape? = null,
-): Modifier = composed {
-    placeholder(
-        visible = visible,
-        shape = shape,
-        highlight = PlaceholderHighlight.shimmer(),
-    )
-}
+): Modifier = placeholder(
+    visible = visible,
+    shape = shape,
+    highlight = PlaceholderHighlight.shimmer(
+        highlightColor = PlaceholderDefaults.shimmerHighlightColor(
+            alpha = 0.33f,
+        ),
+    ),
+)
